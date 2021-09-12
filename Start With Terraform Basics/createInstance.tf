3a64f9ef413e6dd4676f5316097431b9b18e4523
snayineni devops <snayineni@gmail.com>
2021-09-12 14:49:38 -0400

    master

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,5 +1,5 @@
 
-resource "aws_instance" "MyFirstInstnacess" {
+resource "aws_instance" "MyFirstInstnacesss" {
   count         = 3
   ami           = "ami-0bff25b43a4479334"
   instance_type = "t4g.micro"
